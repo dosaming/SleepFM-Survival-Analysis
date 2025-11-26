88ad5a6c59862331ef38ee5f97c7fa8662a2b5bb
dosaming <159415203+dosaming@users.noreply.github.com>
2025-11-26 13:25:31 +0900

    Create config.py

---
+++ sleepfm-reproduction/PhysioNet/config.py
@@ -0,0 +1,95 @@
+
+import configparser
+import os
+import types
+
+_FILENAME = None
+_PARAM = {}
+
+CONFIG = types.SimpleNamespace(
+    FILENAME=_FILENAME,
+    DATASETS=_PARAM.get("datasets", "datasets"),
+    OUTPUT=_PARAM.get("output", "output"),
+    CACHE=_PARAM.get("cache", ".cache"),
+)
+
+#define the paths
+PATH_TO_RAW_DATA = "/ssd/datasets/sleep/physionet.org/files/challenge-2018/1.0.0"
+PATH_TO_PROCESSED_DATA = "/ssd/kdpark/sleepfm-codebase/physionet_segments"
+
+DATASET_PICKLE_PATH = "/ssd/kdpark/sleepfm-codebase/physionet_segments/dataset.pickle"
+DATASET_EVENTS_PATH = "/ssd/kdpark/sleepfm-codebase/physionet_segments/dataset_events_-1.pickle"
+
+EMBED_SAVE_PATH = "/ssd/kdpark/sleepfm-codebase/outputs/my_run/embeddings"
+
+# Define Sleep related global variables
+
+LABELS_DICT = {
+    "Wake": 0, 
+    "Stage 1": 1, 
+    "Stage 2": 2, 
+    "Stage 3": 3, 
+    "REM": 4
+}
+INV_LABELS_DICT = {v: k for k, v in LABELS_DICT.items()}
+
+MODALITY_TYPES = ["respiratory", "sleep_stages", "ekg"]
+CLASS_LABELS = ["Wake", "Stage 1", "Stage 2", "Stage 3", "REM"]
+NUM_CLASSES = 5
+
+EVENT_TO_ID = {
+    "Wake": 1, 
+     "Stage 1": 2, 
+     "Stage 2": 3, 
+     "Stage 3": 4, 
+     "Stage 4": 4, 
+     "REM": 5,
+}
+
+LABEL_MAP = {
+    "Sleep stage W": "Wake", 
+    "Sleep stage N1": "Stage 1", 
+    "Sleep stage N2": "Stage 2", 
+    "Sleep stage N3": "Stage 3", 
+    "Sleep stage R": "REM", 
+    "W": "Wake", 
+    "N1": "Stage 1", 
+    "N2": "Stage 2", 
+    "N3": "Stage 3", 
+    "REM": "REM", 
+    "wake": "Wake", 
+    "nonrem1": "Stage 1", 
+    "nonrem2": "Stage 2", 
+    "nonrem3": "Stage 3", 
+    "rem": "REM", 
+}
+
+
+# Define the channels in your dataset
+ALL_CHANNELS = ['F3-M2',
+ 'F4-M1',
+ 'C3-M2',
+ 'C4-M1',
+ 'O1-M2',
+ 'O2-M1',
+ 'E1-M2',
+ 'Chin1-Chin2',
+ 'ABD',
+ 'CHEST',
+ 'AIRFLOW',
+ 'SaO2',
+ 'ECG']
+
+
+CHANNEL_DATA = {
+    "Respiratory": ["CHEST", "SaO2", "ABD"],
+    "Sleep_Stages": ["C3-M2", "C4-M1", "O1-M2", "O2-M1", "E1-M2"],
+    "EKG": ["ECG"], 
+    }
+
+
+CHANNEL_DATA_IDS = {
+    "Respiratory": [ALL_CHANNELS.index(item) for item in CHANNEL_DATA["Respiratory"]], 
+    "Sleep_Stages": [ALL_CHANNELS.index(item) for item in CHANNEL_DATA["Sleep_Stages"]], 
+    "EKG": [ALL_CHANNELS.index(item) for item in CHANNEL_DATA["EKG"]], 
+ }
